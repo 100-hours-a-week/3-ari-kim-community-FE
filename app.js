d90d8123f3c59098bfa9a21ebeb7dcc16ddd6a44
dvin-22 <dain5835@gmail.com>
2025-12-09 16:20:54 +0900

    fix: FOUC 문제 완화

--- app.js
+++ app.js
@@ -9,15 +9,42 @@ app.use('/client.js', express.static(path.join(__dirname, 'client.js')));
 app.use('/index.css', express.static(path.join(__dirname, 'index.css')));
 
 // --- 페이지 라우팅 ---
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/postList/PostListPage.html')));
-app.get('/posts', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/postList/PostListPage.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/login/LoginPage.html')));
-app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/signup/SignupPage.html')));
-app.get('/posts/create', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/postCreate/PostCreatePage.html')));
-app.get('/posts/:id/edit', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/postUpdate/PostUpdatePage.html')));
-app.get('/posts/:id', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/postDetail/PostDetailPage.html')));
-app.get('/users/:id/edit', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/userUpdate/UserUpdatePage.html')));
-app.get('/users/:id/password', (req, res) => res.sendFile(path.join(__dirname, 'public/pages/passwordUpdate/PasswordUpdatePage.html')));
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/postList/PostListPage.html'));
+});
+app.get('/posts', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/postList/PostListPage.html'));
+});
+app.get('/login', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/login/LoginPage.html'));
+});
+app.get('/signup', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/signup/SignupPage.html'));
+});
+app.get('/posts/create', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/postCreate/PostCreatePage.html'));
+});
+app.get('/posts/:id/edit', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/postUpdate/PostUpdatePage.html'));
+});
+app.get('/posts/:id', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/postDetail/PostDetailPage.html'));
+});
+app.get('/users/:id/edit', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/userUpdate/UserUpdatePage.html'));
+});
+app.get('/users/:id/password', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.sendFile(path.join(__dirname, 'public/pages/passwordUpdate/PasswordUpdatePage.html'));
+});
 
 // --- 서버 실행 ---
 // 테스트 환경이 아닐 때만 서버 실행
